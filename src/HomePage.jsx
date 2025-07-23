2fcbfdc841cde00a54c0efb45b5cfbc54813dfad
luca111111 <66541826+luca111111@users.noreply.github.com>
2025-07-23 23:09:45 +0900

    업데이트 내용 메시지 작성

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -8,7 +8,7 @@ import About from "./pages/About";
 import DeliveryPage from "./pages/delivery";
 import StoreDetail from "./pages/delivery/[id]";
 import MapInfo from "./pages/MapInfo";
-
+  
 export default function HomePage() {
   const [showSplash, setShowSplash] = useState(false);
   const [nextRoute, setNextRoute] = useState(null);
