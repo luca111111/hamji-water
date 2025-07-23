801ee9ff96e6b319c9f582a7365c27ce41fc070d
luca111111 <66541826+luca111111@users.noreply.github.com>
2025-07-24 00:35:47 +0900

    feat: 프로그램 페이지 추가

--- src/pages/nearby/NearbyPage.jsx
+++ src/pages/nearby/NearbyPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function NearbyPage() {
   return (
-    <div className="pt-[80px] px-4 bg-white min-h-screen text-center">
+    <div className="pt-[90px] px-4 bg-white min-h-screen text-center">
       <h1 className="text-2xl font-bold text-blue-600 mb-6 font-hamji">함지맛지도 (근처맛집)</h1>
       <img
         src="/nearby.jpg"
