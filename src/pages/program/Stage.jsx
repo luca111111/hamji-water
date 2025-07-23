81c71ccfb820c535f8ee39aecbf82bcb03f99ade
luca111111 <66541826+luca111111@users.noreply.github.com>
2025-07-23 23:30:31 +0900

    업데이트 내용 메시지 작성

--- src/pages/program/Stage.jsx
+++ src/pages/program/Stage.jsx
@@ -5,7 +5,7 @@ export default function Stage() {
     <div className="pt-[80px] px-4 bg-white min-h-screen text-center">
       <h1 className="text-2xl font-bold text-blue-600 mb-6 font-hamji">메인무대 프로그램</h1>
       <img
-        src="/public/stage.jpg"
+        src="/stage.jpg"  // ❗ 앞에 public 쓰면 안됨!
         alt="무대 프로그램 이미지"
         className="mx-auto rounded-xl shadow-lg max-w-full h-auto"
         loading="lazy"
