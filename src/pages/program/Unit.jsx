413f419c96cc4c3b4f0fd7636540e0e8fc37cb04
luca111111 <66541826+luca111111@users.noreply.github.com>
2025-07-23 23:56:06 +0900

    feat: 프로그램 페이지 추가

--- src/pages/program/Unit.jsx
+++ src/pages/program/Unit.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-export default function Rest() {
+export default function Unit() {
   return (
-    <div className="pt-[40px] px-4 bg-white min-h-screen text-center">
-      <h1 className="text-2xl font-bold text-blue-600 mb-6 font-hamji">휴식존</h1>
+    <div className="pt-[30px] px-4 bg-white min-h-screen text-center">
+      <h1 className="text-2xl font-bold text-blue-600 mb-6 font-hamji">부대 프로그램</h1>
       <img
-        src="/rest.jpg"
-        alt="휴식존 이미지"
+        src="/unit.jpg"
+        alt="부대프로그램 이미지"
         className="mx-auto rounded-xl shadow-lg max-w-full h-auto"
         loading="lazy"
       />
